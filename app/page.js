b16747485166a1cc20c3639582fe7bc16d04dc3a
Antony Araujo <contato.antonyaraujo@gmail.com>
2023-05-11 11:35:10 -0300

    Artualiacao

--- app/page.js
+++ app/page.js
@@ -75,7 +75,6 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <SearchBar />
       <FilterableMessagesTable messages={blogMessages} />
     </main>
   );
